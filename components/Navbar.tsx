abe6279cc655673b8a5b31c82d94ddb5397e306a
Afzal Zubair <49808043+afzalzbr@users.noreply.github.com>
2023-09-07 13:09:17 +0100

    done with layout section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+import { CustomButton } from ".";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <section className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/logo.svg"
+            alt="Car Rental Logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
 
-export default Navbar
+        <CustomButton
+          title="Sign In"
+          containerStyles="bg-primary-blue rounded-full bg-white min-w-[130px]"
+          btnType="button"
+        />
+      </nav>
+    </section>
+  );
+};
+
+export default Navbar;
