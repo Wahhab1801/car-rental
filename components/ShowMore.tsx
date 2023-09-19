0a13ead757822177bf647393d6154ab3b1cee668
Afzal Zubair <49808043+afzalzbr@users.noreply.github.com>
2023-09-19 22:39:39 +0100

    fixed show more button

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -10,17 +10,17 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
 
   const handleNavigation = () => {
-    const newPathname = updateSearchParams(
-      "pageNumber",
-      (pageNumber + 1).toString()
-    );
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
     router.push(newPathname);
   };
   return (
     <div className="w-full flex-center gap=5 mt-10">
-      {isNext && (
+      {!isNext && (
         <CustomButton
+          btnType="button"
           title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
           handleClick={handleNavigation}
         />
       )}
