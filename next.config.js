8d5c9edc738204c5026f732f21352d993bc94f34
Sameer Ahmad <sameerahmad@Sameers-MacBook-Pro.local>
2023-11-13 13:05:10 +0000

    cors

--- next.config.js
+++ next.config.js
@@ -1,18 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        domains: ['images.unsplash.com', 'res.cloudinary.com', 'cdn.sanity.io','cdn.shopify.com']
-    },
-    // async redirects() {
-    //     return [
-    //     {
-    //         source: '/blog',
-    //         destination: '/',
-    //         permanent: true,
-    //     },
-    //     ]
-    // },
-}
+  reactStrictMode: false,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "res.cloudinary.com",
+      "cdn.sanity.io",
+      "cdn.shopify.com",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  // async redirects() {
+  //     return [
+  //     {
+  //         source: '/blog',
+  //         destination: '/',
+  //         permanent: true,
+  //     },
+  //     ]
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
